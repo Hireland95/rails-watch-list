53f2fd0e80797d2aa6b1716f9410769e51ff3abf
Henry Ireland <henryireland95@gmail.com>
2023-08-18 14:09:19 +0100

    deployment changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,7 @@ url = URI("http://tmdb.lewagon.com/movie/top_rated?api_key=<your_api_key>")
 
 movies_serialized = URI.open(url).read
 movies = JSON.parse(movies_serialized)
-c
+
 movies["results"].each do | result |
   Movie.create(title: result["original_title"], overview: result["overview"], poster_url: "https://image.tmdb.org/t/p/original/#{result["poster_path"]}")
 end
